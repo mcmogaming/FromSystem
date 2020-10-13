7b8022a818f1d88b230ade1d72e7cad344064dce
BuildTools <unconfigured@null.spigotmc.org>
2020-10-13 07:54:27 -0400

    just testing out some things

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const home = express();
 
 app.get('/', function(req, res) {
     res.send('hello world')
 })
+
+home.get('/', function(req, res) {
+    res.send('this my home!');
+})
+
+app.use('/app');
+home.use(['/home', '/page'], app);
 
 app.listen(3000);
